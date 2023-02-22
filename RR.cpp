76536dc739f55a3cc2a57fa6ef186208b3e36796
mrugendrakul <mrugendraskulkarni@gmail.com>
2023-02-22 12:18:29 +0530

    ff

--- RR.cpp
+++ RR.cpp
@@ -9,12 +9,25 @@ class process{
     process();
 };
 
-
+void roundRobin(process p[],int tq){
+    
+}
 
 int main(){
     int n;
     cout<<"give number of process:";
     cin >>n;
     process p[n];
-    
+    cout<<"arival times:\n";
+    for(int i=0;i<n;i++){
+        cin>>p[i].at;
+    }
+    cout<<"birst times: \n";
+    for(int i=0;i<n;i++){
+        cin>>p[i].bt;
+    }
+    int tq=0;
+    cout<<"time quantum";
+    cin>>tq;
+    roundRobin(p,tq);
 }

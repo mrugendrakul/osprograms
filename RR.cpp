8429673178adc53571de2dde878849ba42986137
mrugendrakul <mrugendraskulkarni@gmail.com>
2023-02-22 12:43:40 +0530

    RR

--- RR.cpp
+++ RR.cpp
@@ -4,13 +4,53 @@ using namespace std;
 
 class process{
     public:
-    int id,at,bt;
+    int id,at,bt,ct;
 
     process();
 };
 
-void roundRobin(process p[],int tq){
+
+
+void roundRobin(process p[],int tq,int n){
+    vector<process> gantt;
+    queue<process> rq;
+    int cot=0;
+    rq.push(p[0]);
     
+    while(rq.empty()){
+        process temp=rq.front();
+        cout<<"current process in rq is: "<<temp.id<<endl;
+        gantt.push_back(temp);
+        rq.pop();
+        if(temp.bt>tq){
+            temp.bt-=tq;
+        }
+        else{
+            temp.bt=0;
+        }
+        for(int i=0;i<n;i++){
+            if(p[i].at<=cot){
+                rq.push(p[i]);
+            }
+        }
+        if(temp.bt!=0){
+            rq.push(temp);
+        }
+    }
+}
+
+void sortByAt(process p[],int n){
+    int min_idx=0;
+    for(int i=0;i<n;i++){
+        min_idx=i;
+        for(int j=i+1;j<n;j++){
+            if(p[j].at<p[min_idx].at){
+                process temp=p[j];
+                p[j]=p[min_idx];
+                p[min_idx]=temp;
+            }
+        }
+    }
 }
 
 int main(){
@@ -18,6 +58,9 @@ int main(){
     cout<<"give number of process:";
     cin >>n;
     process p[n];
+    for(int i=0;i<n;i++){
+        p[i].id=i;
+    }
     cout<<"arival times:\n";
     for(int i=0;i<n;i++){
         cin>>p[i].at;
@@ -29,5 +72,6 @@ int main(){
     int tq=0;
     cout<<"time quantum";
     cin>>tq;
-    roundRobin(p,tq);
+    sortByAt(p,n);
+    roundRobin(p,tq,n);
 }

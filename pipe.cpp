bdf5108800ced0da59e105d576df66919537954c
mrugendrakul <mrugendraskulkarni@gmail.com>
2023-03-14 15:58:05 +0530

    piping between the parent and child process

--- pipe.cpp
+++ pipe.cpp
@@ -1,37 +1,42 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<unistd.h>
 
 int main() {
-    int pipefd[2];
-    pid_t cpid;
-    char buf;
-
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        return 1;
-    }
-
-    cpid = fork();
-    if (cpid == -1) {
-        perror("fork");
-        return 1;
-    }
-
-    if (cpid == 0) { // Child reads from pipe
-        close(pipefd[1]); // Close unused write end
-
-        while (read(pipefd[0], &buf, 1) > 0)
-            write(STDOUT_FILENO, &buf, 1);
-
-        write(STDOUT_FILENO, "\n", 1);
-        close(pipefd[0]);
-        _exit(0);
-    } else { // Parent writes to pipe
-        close(pipefd[0]);
-        write(pipefd[1], "Hello from parent!", 18);
-        close(pipefd[1]); // Reader will see EOF
-        wait(NULL); // Wait for child
-        return 0;
-    }
+   int pipefds1[2], pipefds2[2];
+   int returnstatus1, returnstatus2;
+   int pid;
+   char pipe1writemessage[20] = "Hi";
+   char pipe2writemessage[20] = "Hello";
+   char readmessage[20];
+   returnstatus1 = pipe(pipefds1);
+   
+   if (returnstatus1 == -1) {
+      printf("Unable to create pipe 1 \n");
+      return 1;
+   }
+   returnstatus2 = pipe(pipefds2);
+   
+   if (returnstatus2 == -1) {
+      printf("Unable to create pipe 2 \n");
+      return 1;
+   }
+   pid = fork();
+   
+   if (pid != 0) // Parent process 
+   {
+      close(pipefds1[0]); // Close the unwanted pipe1 read side
+      close(pipefds2[1]); // Close the unwanted pipe2 write side
+      printf("In Parent: Writing to pipe 1 – Message is %s\n", pipe1writemessage);
+      write(pipefds1[1], pipe1writemessage, sizeof(pipe1writemessage));
+      read(pipefds2[0], readmessage, sizeof(readmessage));
+      printf("In Parent: Reading from pipe 2 – Message is %s\n", readmessage);
+   } else { //child process
+      close(pipefds1[1]); // Close the unwanted pipe1 write side
+      close(pipefds2[0]); // Close the unwanted pipe2 read side
+      read(pipefds1[0], readmessage, sizeof(readmessage));
+      printf("In Child: Reading from pipe 1 – Message is %s\n", readmessage);
+      printf("In Child: Writing to pipe 2 – Message is %s\n", pipe2writemessage);
+      write(pipefds2[1], pipe2writemessage, sizeof(pipe2writemessage));
+   }
+   return 0;
 }
